d4a5b70ebe84cc204fa7c2817a70683d0bc616a6
overbejt <overbejt@miamioh.edu>
2019-04-16 21:59:29 -0400

    Created a package to hold the tester files.  Also created a tester class for the Attractions class.

---
+++ Tests/AttractionsTest.java
@@ -0,0 +1,39 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This is the class that will test the Attractions class
+ * 
+ * @author Josh Overbeck
+ */
+class AttractionsTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+} // End of the 'AttractionsTest' class
