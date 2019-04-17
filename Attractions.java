168d51add35a0dcea40328aecf9a98a5a0ec8156
overbejt <overbejt@miamioh.edu>
2019-04-16 21:10:43 -0400

    created the attractions class

---
+++ Attractions.java
@@ -0,0 +1,11 @@
+/*
+ * @author Josh Overbeck
+ */
+public class Attractions {
+
+	// Default constructor
+	public Attractions() {
+
+	} // End of the default constructor
+
+} // End of the 'Attractions' class
