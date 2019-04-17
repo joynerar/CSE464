9979941d25fc9e3f516c5622f02b93eb59a0a9fa
overbejt <overbejt@miamioh.edu>
2019-04-16 22:41:02 -0400

    Created a test to check the getNeighborsTest method

--- AttractionsTest.java
+++ AttractionsTest.java
@@ -1,4 +1,7 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.HashMap;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -27,5 +30,46 @@ class AttractionsTest {
 		destination = new Attractions();
 		assertNotNull(destination);
 	} // End of the 'basicTest' method
+
+	@Test
+	void getNameTest() {
+		String name = "beast";
+		destination.setName(name);
+		assertEquals(name, destination.getName());
+	} // End of the 'getNameTest' method
+
+	@Test
+	void getWaitTimeTest() {
+		int expectedWaitTime = 15;
+		destination.setWaitTime(expectedWaitTime);
+		assertEquals(expectedWaitTime, destination.getWaitTime());
+	} // End of the 'getWaitTimeTest' method
+
+	@Test
+	void getRideTimeTest() {
+		int expectedRideTime = 15;
+		destination.setWaitTime(expectedRideTime);
+		assertEquals(expectedRideTime, destination.getRideTime());
+	} // End of the 'expectedRideTime' method
+
+	@Test
+	void getNeighborsTest() {
+
+		// Creating a HashMap of neighbors
+		HashMap<Attractions, Integer> expectedNeighbors = new HashMap();
+
+		// Creating a neighbor
+		Attractions neighbor = new Attractions();
+		neighbor.setName("ironMan");
+		neighbor.setRideTime(15);
+		neighbor.setWaitTime(25);
+
+		// Adding the neighbor to 'expectedNeighbors'
+		expectedNeighbors.put(neighbor, 5);
+		// Adding the neighbor to 'destination'
+		destination.addNeighbor(neighbor, 5);
+
+		assertEquals(expectedNeighbors, destination.getNeighbors());
+	} // End of the 'getNeighborsTest' method
 
 } // End of the 'AttractionsTest' method
