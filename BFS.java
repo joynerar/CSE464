2521bc65568ee7d5e892c8d454b5b5af0fb5d233
overbejt <overbejt@miamioh.edu>
2019-04-18 17:42:31 -0400

    Created the BFS class file

---
+++ BFS.java
@@ -0,0 +1,13 @@
+/**
+ * This is the class that will handle the BFS algorithm.
+ * 
+ * @author Josh Overbeck
+ */
+public class BFS {
+
+	// Default Constructor
+	public BFS() {
+
+	} // End of the Default Constructor
+
+} // End of the 'BFS' class
