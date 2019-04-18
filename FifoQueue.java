a926cecb78144ded0ece1124eff2ebf92655d2af
overbejt <overbejt@miamioh.edu>
2019-04-18 15:54:25 -0400

    Finished writing the setters and getters for the nested Node class

--- FifoQueue.java
+++ FifoQueue.java
@@ -23,18 +23,88 @@ public class FifoQueue {
 		// Default Constructor
 		public Node() {
 			// Initializing the instance variables
-			left = null;
-			right = null;
-			data = new Attractions();
+			this.left = null;
+			this.right = null;
+			this.data = new Attractions();
 		} // End of the Default constructor
 
 		// Overriding the Default Constructor
 		public Node(Attractions input) {
 			// Initializing the instance variables
-			left = null;
-			right = null;
-			data = input;
+			this.left = null;
+			this.right = null;
+			this.data = input;
 		} // End of the Override Default constructor
+
+		// *********************** Setters ***************************
+
+		/**
+		 * This is the method that will set the pointer of the node to
+		 * the left node.
+		 * 
+		 * @param input
+		 *            - The node on the left that will be pointed to
+		 *            by the 'left' pointer.
+		 */
+		public void setLeft(Node input) {
+			this.left = input;
+		} // End of the 'setLeft' method
+
+		/**
+		 * This is the method that will set the pointer of the node to
+		 * the right node.
+		 * 
+		 * @param input
+		 *            - The node on the right that will be pointed to
+		 *            by the 'right' pointer.
+		 */
+		public void setRight(Node input) {
+			this.right = input;
+		} // End of the 'setRight' method
+
+		/**
+		 * This is the method that will set the data associated with
+		 * this node.
+		 * 
+		 * @param input
+		 *            - The data that you want to associate with this
+		 *            node.
+		 */
+		public void setData(Attractions input) {
+			this.data = input;
+		} // End of the 'setData' method
+
+		// *********************** Getters ***************************
+
+		/**
+		 * This is the method for retrieving the node to the left of
+		 * this node.
+		 * 
+		 * @return - The node to the left.
+		 */
+		public Node getLeft() {
+			return this.left;
+		} // End of the 'getLeft' method
+
+		/**
+		 * This is the method for retrieving the node to the right of
+		 * this node.
+		 * 
+		 * @return - The node to the right.
+		 */
+		public Node getRight() {
+			return this.right;
+		} // End of the 'getRight' method
+
+		/**
+		 * This is the method for retrieving the data associated with
+		 * this node.
+		 * 
+		 * @return - The data associated with this node.
+		 */
+		public Attractions getData() {
+			return this.data;
+		} // End of the 'getData' method
 
 	} // End of the 'Node' class
 
