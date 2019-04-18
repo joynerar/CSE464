35655a06efde16d279d1606fe6a77dfd3a2d6ccc
overbejt <overbejt@miamioh.edu>
2019-04-18 15:42:59 -0400

    Ovverode the default constructor in the node class

--- FifoQueue.java
+++ FifoQueue.java
@@ -15,6 +15,27 @@ public class FifoQueue {
 	 */
 	class Node {
 
+		// Declaring instance variables
+		Node		left;
+		Node		right;
+		Attractions	data;
+
+		// Default Constructor
+		public Node() {
+			// Initializing the instance variables
+			left = null;
+			right = null;
+			data = new Attractions();
+		} // End of the Default constructor
+
+		// Overriding the Default Constructor
+		public Node(Attractions input) {
+			// Initializing the instance variables
+			left = null;
+			right = null;
+			data = input;
+		} // End of the Override Default constructor
+
 	} // End of the 'Node' class
 
 } // End of the 'FifoQueue' class
