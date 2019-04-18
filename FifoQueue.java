63ce8a36344437ed3fb5f87102689da74aae865c
overbejt <overbejt@miamioh.edu>
2019-04-18 15:27:37 -0400

    Generated the FifoQueue class

---
+++ FifoQueue.java
@@ -0,0 +1,11 @@
+/**
+ * @author Josh Overbeck
+ */
+public class FifoQueue {
+
+	// Default constructor
+	public FifoQueue() {
+
+	} // End of the Default Constructor
+
+} // End of the 'FifoQueue' class
