470dc3c1370b4db46f60d384b496006ee9363980
Adam Joyner <36858035+joynerar@users.noreply.github.com>
2019-04-25 17:34:25 -0400

    Implemented routToNext Method

--- Driver.java
+++ Driver.java
@@ -4,10 +4,21 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 public class Driver {
+	static BFS bfs;
 
-	private static boolean hasTime() {
+	private static ArrayList<Attractions> checkRouteToDest(Attractions current, Attractions destination,
+			int timeRemaining, BFS bfs) {
+		ArrayList<Attractions> routeToNext = bfs.getPath(destination, current);
+		int routeToNextTime = bfs.getTime();
 
-		return false;
+		if (routeToNextTime < timeRemaining) {
+			bfs.getPath(destination, bfs.ridemap.get(0));
+			int timeToExit = bfs.getTime();
+			if (routeToNextTime + timeToExit < timeRemaining) {
+				return routeToNext;
+			}
+		}
+		return new ArrayList<Attractions>();
 	}
 
 	private static Attractions getSmallestNeighbor() {
@@ -32,26 +43,35 @@ public class Driver {
 		// Current position for creating schedule
 		Attractions currentPosition = parkMap.get("ENTRANCE");
 		int rideCounter = 0; // L
-		BFS bfs = new BFS(new ArrayList<Attractions>(parkMap.values()));
+		bfs = new BFS(new ArrayList<Attractions>(parkMap.values()));
 		Attractions dest = null;
 		int remainingTime = 0;
 		while (!currentPosition.getName().equals("ENTRANCE")) {
+
 			remainingTime = timeAllotted - totalTime;
+
 			// Checks if all user prefs have been done yet and theres enough time
-			if (bfs.checkTime(remainingTime)) {
-				if (rideCounter < userDestinations.size())
-					dest = parkMap.get(userDestinations.get(mapKeys.get(rideCounter)));
-				else
-					dest = getSmallestNeighbor();
+			/*
+			 * if (hasTime()) { if (rideCounter < userDestinations.size()) dest =
+			 * parkMap.get(userDestinations.get(mapKeys.get(rideCounter))); else dest =
+			 * getSmallestNeighbor(); } else { dest = parkMap.get("ENTRANCE"); }
+			 */
+
+			if (rideCounter < userDestinations.size()) {
+				dest = parkMap.get(userDestinations.get(mapKeys.get(rideCounter)));
 			} else {
+				dest = getSmallestNeighbor();
+			}
+			ArrayList<Attractions> routeToDest = checkRouteToDest(currentPosition, dest, remainingTime, bfs);
+			if (routeToDest.size() == 0) {
 				dest = parkMap.get("ENTRANCE");
+				bfs.getPath(currentPosition, bfs.ridemap.get(0));
 			}
 
-			ArrayList<Attractions> tmp = bfs.getPath(dest, currentPosition, remainingTime);
+			for (int i = 1; i < routeToDest.size(); i++)
+				route.add(routeToDest.get(i));
 
-			for (int i = 1; i < tmp.size(); i++)
-				route.add(tmp.get(i));
-
+			totalTime += bfs.getTime();
 			currentPosition = dest;
 			rideCounter++;
 		}
