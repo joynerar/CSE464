8b0e1c83771c00f784bd2d26abfb75e56d3ffd4c
overbejt <overbejt@miamioh.edu>
2019-04-30 20:44:07 -0400

    driver is started

--- Dr2.java
+++ Dr2.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Dr2 {
+	// Instance variables
+	static BFS							bfs;
+	static IO							io;
+	static HashMap<String, Attractions>	parkMap;
+	static HashMap<String, Attractions>	userDestinations;
+	static ArrayList<Attractions>		map;
 
-	static BFS bfs;
-
+	// main method
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Initialize instance variables
+		io = new IO("map2.txt", "user2.txt", "connect2.txt",
+				"outputfile.txt");
+		parkMap = io.getAttractionList();
+		userDestinations = io.getUserPrefs();
+		map = new ArrayList<Attractions>(parkMap.values());
+		bfs = new BFS(map);
+
+		System.out.println("So far so good");
 
 	} // End of the 'main' method
 
